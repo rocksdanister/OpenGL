8de1a7063db82344b6e4147b7d8c47f0a28cd255
Dani John <rocksdanister@users.noreply.github.com>
2017-01-23 21:39:59 +0530

    Update bullet_wall.cpp

--- LabWorkOut/bullet_wall.cpp
+++ LabWorkOut/bullet_wall.cpp
@@ -312,7 +312,7 @@ int main(int argc, char **argv)
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(700, 700);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("OpenGL - Rotating a Cube");
+    glutCreateWindow("Test v1");
     initRendering();
     glutDisplayFunc(drawCube);
 	  glutIdleFunc(mixedStepLoop);
