c47e1b8b9236c949262742d1e06bf635b422670a
Dani John <rocksdanister@users.noreply.github.com>
2017-02-22 20:19:32 +0530

    Add files via upload

---
+++ modelLoader/build1/myheader.h
@@ -0,0 +1,24 @@
+extern double queuedMilliseconds,prev0,fps,responseTime;
+extern float lx,lz,x,z,angle;
+extern GLuint dlist;
+
+void mixedStepLoop();
+void processSpecialKeys(int , int , int );
+void draw();
+void initialOBJ();
+void objDraw();
+void optimisationList();
+
+
+class data
+{
+public:
+float v[3];
+float vn[3];
+float tex[3];
+
+data()
+	{
+		v[0]=0;v[1]=0;v[2]=0;vn[0]=0;vn[1]=0;vn[2]=0;tex[0]=0;tex[1]=0;tex[2]=0;
+	}
+};
