bb0f0659f5f23bf1f0de1cf6c39c524a70026fb4
Dani John <rocksdanister@users.noreply.github.com>
2017-02-25 08:14:34 +0530

    Update draw.cpp

--- Games/droneX/draw.cpp
+++ Games/droneX/draw.cpp
@@ -42,7 +42,7 @@ void init()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	image = SOIL_load_image( "res/terrains/sidescroll_lowres3.png", &width, &height, 0, SOIL_LOAD_RGBA );
+	image = SOIL_load_image( "res/terrains/sidescroll.png", &width, &height, 0, SOIL_LOAD_RGBA );
 	glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image );
 	SOIL_free_image_data( image );
 
