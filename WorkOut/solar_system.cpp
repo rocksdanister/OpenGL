516c05b57cee932271cf1922a3f8c5b1928c1cf1
Dani John <rocksdanister@gmail.com>
2017-04-18 17:37:22 +0530

    Update solar_system.cpp

--- WorkOut/solar_system.cpp
+++ WorkOut/solar_system.cpp
@@ -9,11 +9,23 @@ using namespace std;
 
 GLdouble radius=1;
 float jj,angle = 0.0;
-GLfloat light0_position[] = {0.0,0.0,1.0, 0.0};
+GLfloat light0_position[] = {0.0,0.0,5, 1.0};
 GLfloat white[]={1.0f,1.0f,1.0f,1.f};
+GLfloat red[]={0.9,0.1,0,1};
 GLfloat objcolor[]={1.0f,0.0f,0.0f,1.0f}; 
 GLfloat suncolor[]={ 0.98f, 0.834f, 0.25f ,1.0f};
+GLfloat planetColor[8][3]={{0.7,0.7,0.7},
+						   {1,1,0},
+						   {0,0,0.8},
+						   {1,0,0},
+						   {0.5,0.5,0.5},
+						   {0.7,0.7,0},
+						   {0,0,1},
+						   {0,0,0.7}
+						  };
 int ii;
+GLfloat planetRadius[8]={0.2,0.3,0.35,0.25,0.78,0.69,0.65,0.6};
+GLfloat planetAngle[8];
 
 void handleResize(int w, int h)
 {
@@ -33,8 +45,8 @@ void init()
     glShadeModel(GL_SMOOTH); 
     glLightfv(GL_LIGHT0, GL_POSITION, light0_position); 
     glLightfv(GL_LIGHT0,GL_DIFFUSE,white);
-    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE,suncolor);
-    //glMateriali(GL_FRONT, GL_SHININESS, 1);
+    glMaterialfv(GL_FRONT_AND_BACK,GL_AMBIENT,white);
+    glMateriali(GL_FRONT, GL_SHININESS, 100);
     glClearColor(0,0,0,1);
 }
 double rX=0;
@@ -68,31 +80,29 @@ void draw()
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glPushMatrix();
+    gluLookAt(0,-20,5,0.5,0.5,-0.1,0,1,0);
     glLightfv(GL_LIGHT0, GL_POSITION, light0_position);
-    glTranslatef(0.0, 0.0, -7.0);
+    //glTranslatef(0.0, 0.0, -7.0);
     glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE,white);
-    for(jj=0.3,ii=0;ii<8;ii++)
+    for(jj=0.5,ii=0;ii<8;ii++)
     {
-    	jj+=0.3;
+    	jj+=.60;
     	drawCircle(jj);
     }
-    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE,suncolor);
-    //glRotatef(angle, 1.0, 1.0, 1.0);
-    //glRotatef(angle, 1.0, 0.0, 1.0 );
-    //glRotatef(angle, 0.0, 1.0, 1.0 );
-    //glTranslatef(-0.5, -1.0, 0.0);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE,red);
 
     //.... SUN
-    glScalef(.2,.2,.2);
+    glScalef(.4,.4,.4);
     glutSolidSphere(radius,50,50);
-    glRotatef(angle, 0.0, 0.0, 1.0);
     //...PLANETS
     for(jj=3,ii=0;ii<8;ii++)
     {
     	glPushMatrix();
+    	glRotatef(planetAngle[ii], 0.0, 0.0, 1.0);
     	glTranslatef(jj,1,0);
     	jj+=1.5;
-    	glutSolidSphere(0.5,50,50);
+    	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE,planetColor[ii]);
+    	glutSolidSphere(planetRadius[ii],50,50);
     	glPopMatrix();
     }
 
@@ -100,14 +110,17 @@ void draw()
     glutSwapBuffers();
 }
 
+GLfloat planetAdd[8]={0.24,0.62,1,1.00,1.32,1.55,1.88,2};
 void update(int value)
 {
-    angle += 1.0f;
-    if (angle > 360)
-        angle -= 360;
+    //angle += 1.0f;
+    //if (angle > 360)
+      //  angle -= 360;
 
+	for(ii=0;ii<8;ii++)
+	planetAngle[ii]+=planetAdd[ii];
     glutPostRedisplay();
-    glutTimerFunc(16.66, update, 0);
+    glutTimerFunc(16.67, update, 0);
 }
 
 
@@ -129,7 +142,7 @@ int main(int argc, char **argv)
     glutCreateWindow("Solar System");
     init();
     glutDisplayFunc(draw);
-    glutTimerFunc(16.66, update, 0);
+    glutTimerFunc(16.67, update, 0);
     glutReshapeFunc(handleResize);
     glutKeyboardFunc(keyboard); 
     glutMainLoop();
